47af3dacac538807c46332849601f191e2af9d9a
killthistinker <killthistinker@gmail.com>
2022-10-31 07:31:02 +0600

    #7 Создал интерфейс для взаимодействия с операторами

---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Contracts/Services/Interfaces/IOperatorService.cs
@@ -0,0 +1,9 @@
+﻿using UpPhoneBalance.Domain.DataTransfers;
+
+namespace UpPhoneBalance.Infrastructure.Contracts.Services.Interfaces;
+
+public interface IOperatorService
+{
+    public Task<string>? GetOperatorName(PaymentDto payment);
+    public string SetPaymentToOperator(string operatorName, PaymentDto payment);
+}
