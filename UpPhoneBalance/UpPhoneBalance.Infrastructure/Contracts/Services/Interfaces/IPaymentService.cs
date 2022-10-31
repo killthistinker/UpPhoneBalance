93e1f073bc57cc996ec3749629f5ab59a159fe5e
killthistinker <killthistinker@gmail.com>
2022-10-31 07:27:51 +0600

    #3 Создал интерфейс для сервиса платежей

---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Contracts/Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Result;
+using UpPhoneBalance.Domain.DataTransfers;
+using UpPhoneBalance.Infrastructure.Models.Responses;
+
+namespace UpPhoneBalance.Infrastructure.Contracts.Services.Interfaces;
+
+public interface IPaymentService
+{
+    public Task<Result<DefaultResponseObject<PaymentResponse>>> PaymentManager(PaymentDto model);
+}
