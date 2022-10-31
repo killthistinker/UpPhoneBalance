8908e4a2edb1945c13c039eb19006e448e0cc91a
killthistinker <killthistinker@gmail.com>
2022-10-31 07:34:58 +0600

    #3 Настроил DependencyInjection

---
+++ UpPhoneBalance/UpPhoneBalance.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Globalization;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using UpPhoneBalance.Application.Mappings;
+using UpPhoneBalance.Application.Patterns.Strategy.Interfaces;
+using UpPhoneBalance.Application.Services;
+using UpPhoneBalance.Infrastructure.Contracts.Services.Interfaces;
+
+namespace UpPhoneBalance.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddTransient<IPaymentService, PaymentService>();
+        serviceCollection.AddTransient<IOperatorService, OperatorService>();
+        serviceCollection.AddAutoMapper(typeof(PaymentMapper), typeof(DefaultResponseObjectProfile));
+        serviceCollection.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        serviceCollection.AddLocalization(options => options.ResourcesPath = "Resources");
+        return serviceCollection;
+    }
+}
---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/DependencyOnjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UpPhoneBalance.Infrastructure.Contracts.Repository;
+using UpPhoneBalance.Infrastructure.Contracts.Repository.Interfaces;
+using UpPhoneBalance.Infrastructure.Contracts.Services.Interfaces;
+using UpPhoneBalance.Infrastructure.Data;
+
+namespace UpPhoneBalance.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connection = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<PhonePaymentsDbContext>(options => options.UseNpgsql(connection));
+        services.AddTransient<IPaymentRepository, PaymentRepository>();
+        services.AddTransient<IPrefixRepository, PrefixRepository>();
+        return services;
+    }
+}
