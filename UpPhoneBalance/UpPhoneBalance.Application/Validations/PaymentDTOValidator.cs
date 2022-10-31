5568656f4e8691a3f573a17ed9560a021483915c
killthistinker <killthistinker@gmail.com>
2022-10-31 07:31:45 +0600

    #3 Настроил валидацию для входящих данных

---
+++ UpPhoneBalance/UpPhoneBalance.Application/Validations/PaymentDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using UpPhoneBalance.Domain.DataTransfers;
+
+namespace UpPhoneBalance.Application.Validations;
+
+public class PaymentDtoValidator : AbstractValidator<PaymentDto>
+{
+    public PaymentDtoValidator(IStringLocalizer<PaymentDtoValidator> localizer)
+    {
+        RuleFor(p => p.Sum).GreaterThan(0).WithMessage(localizer["InvalidValue"]).NotNull()
+            .WithMessage(localizer["RequiredField"]);
+        RuleFor(p => p.PhoneNumber).NotNull().WithMessage(localizer["RequiredField"]).MaximumLength(16).WithMessage(localizer["InvalidFormat"]).MinimumLength(10)
+            .WithMessage(localizer["InvalidFormat"]);
+    }
+}
