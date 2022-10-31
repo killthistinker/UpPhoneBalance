a5f4de15ec7b2e21c483c8a1b915d9a6d5b5b82f
killthistinker <killthistinker@gmail.com>
2022-10-31 07:33:44 +0600

    #7 Реализовал паттерн "Стратегия"

---
+++ UpPhoneBalance/UpPhoneBalance.Application/Patterns/Strategy/Interfaces/IOperatorActions.cs
@@ -0,0 +1,8 @@
+﻿using UpPhoneBalance.Domain.DataTransfers;
+
+namespace UpPhoneBalance.Application.Patterns.Strategy.Interfaces;
+
+public interface IOperatorActions
+{ 
+    public string SetPayment(string payment, PaymentDto model);
+}
---
+++ UpPhoneBalance/UpPhoneBalance.Application/Patterns/Strategy/Operators/Tele2.cs
@@ -0,0 +1,9 @@
+﻿using UpPhoneBalance.Application.Patterns.Strategy.Interfaces;
+using UpPhoneBalance.Domain.DataTransfers;
+
+namespace UpPhoneBalance.Application.Patterns.Strategy.Operators;
+
+public class Tele2 : IOperatorActions
+{
+    public string SetPayment(string payment, PaymentDto model) => $"Платеж оператору {payment}, на сумму {model.Sum} успешно совершен";
+}
