42f902952eb3ab552700e90697d4de3605b93f05
killthistinker <killthistinker@gmail.com>
2022-10-31 07:26:43 +0600

    #4 Реализовал интерфесы репозитория

---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Contracts/Repository/PaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using UpPhoneBalance.Domain.Entities;
+using UpPhoneBalance.Infrastructure.Contracts.Repository.Interfaces;
+using UpPhoneBalance.Infrastructure.Data;
+
+namespace UpPhoneBalance.Infrastructure.Contracts.Repository;
+
+public class PaymentRepository : IPaymentRepository
+{
+    private readonly PhonePaymentsDbContext _context;
+
+    public PaymentRepository(PhonePaymentsDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<PhonePaymentsDbModel>? GetAll() => _context.Payments;
+    public void Create(PhonePaymentsDbModel item) => _context.Payments.Add(item);
+    public void Update(PhonePaymentsDbModel item) => _context.Payments.Update(item);
+    public void Remove(PhonePaymentsDbModel item) => _context.Payments.Remove(item);
+    public void Save() => _context.SaveChanges();
+    public async Task SaveAsync() => await _context.SaveChangesAsync();
+}
---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Contracts/Repository/PrefixRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UpPhoneBalance.Domain.Entities;
+using UpPhoneBalance.Infrastructure.Contracts.Repository.Interfaces;
+using UpPhoneBalance.Infrastructure.Data;
+
+namespace UpPhoneBalance.Infrastructure.Contracts.Repository;
+
+public class PrefixRepository : IPrefixRepository
+{
+    private readonly PhonePaymentsDbContext _context;
+
+    public PrefixRepository(PhonePaymentsDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PrefixesDbModel> GetPrefixByKeyAsync(string key) =>
+        await _context.Prefixes.FirstOrDefaultAsync(p => p.Key == key);
+ 
+    public IEnumerable<PrefixesDbModel>? GetAll() => _context.Prefixes;
+    public void Create(PrefixesDbModel? item) => _context.Prefixes.Add(item);
+    public void Update(PrefixesDbModel? item) => _context.Prefixes.Update(item);
+    public void Remove(PrefixesDbModel? item) => _context.Prefixes.Remove(item);
+    public void Save() => _context.SaveChanges();
+    public async Task SaveAsync() => await _context.SaveChangesAsync();
+
+}
