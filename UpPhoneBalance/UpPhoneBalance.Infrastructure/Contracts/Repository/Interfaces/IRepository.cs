6313ac6ce56962beee02170538ab8e4a116f3ba2
killthistinker <killthistinker@gmail.com>
2022-10-31 07:25:23 +0600

    #4 Настроил интерфейс с методами по умолчанию

---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Contracts/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace UpPhoneBalance.Infrastructure.Contracts.Repository.Interfaces;
+
+public interface IRepository<T>
+{
+    public IEnumerable<T>? GetAll();
+    public void Create(T item);
+    public void Update(T item);
+    public void Remove(T item);
+    public void Save(); 
+    public Task SaveAsync();  
+}
