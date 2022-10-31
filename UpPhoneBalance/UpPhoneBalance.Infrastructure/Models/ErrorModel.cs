033e8abe9c69a73a3b706fe92a7385b2036b2811
killthistinker <killthistinker@gmail.com>
2022-10-31 07:30:17 +0600

    #3 Создал модель с ошибками

---
+++ UpPhoneBalance/UpPhoneBalance.Infrastructure/Models/ErrorModel.cs
@@ -0,0 +1,9 @@
+﻿using UpPhoneBalance.Infrastructure.Enums;
+
+namespace UpPhoneBalance.Infrastructure.Models;
+
+public class ErrorModel
+{
+    public string Message { get; set; }
+    public StatusCodes StatusCode { get; set; }
+}
