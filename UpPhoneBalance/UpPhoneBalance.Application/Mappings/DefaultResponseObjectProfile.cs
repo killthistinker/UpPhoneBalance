e5ab522e9b53790bde7f7f2ba9f5b55bece84c85
killthistinker <killthistinker@gmail.com>
2022-10-31 07:30:03 +0600

    #3 Настроил маппер для ответа

---
+++ UpPhoneBalance/UpPhoneBalance.Application/Mappings/DefaultResponseObjectProfile.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Result;
+using AutoMapper;
+using UpPhoneBalance.Infrastructure.Models.Responses;
+
+namespace UpPhoneBalance.Application.Mappings;
+
+public class DefaultResponseObjectProfile : Profile
+{
+    public DefaultResponseObjectProfile()
+    {
+        CreateMap(typeof(Result<Task<DefaultResponseObject<PaymentResponse>>>), typeof(DefaultResponseObject<PaymentResponse>));
+    }
+}
