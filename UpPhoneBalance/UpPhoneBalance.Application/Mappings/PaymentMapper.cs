2ba2c72b271359aa08c6d8e7c142e746368eb876
killthistinker <killthistinker@gmail.com>
2022-10-31 07:28:10 +0600

    #3 Настроил маппер

---
+++ UpPhoneBalance/UpPhoneBalance.Application/Mappings/PaymentMapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using AutoMapper;
+using UpPhoneBalance.Domain.DataTransfers;
+using UpPhoneBalance.Domain.Entities;
+
+namespace UpPhoneBalance.Application.Mappings;
+
+public class PaymentMapper : Profile
+{
+    public PaymentMapper()
+    {
+        CreateMap<PaymentDto, PhonePaymentsDbModel>()
+            .ForMember(dst => dst.Id, opt => opt.Ignore())
+            .ForMember(dst => dst.Sum, opt => opt.MapFrom(src => src.Sum))
+            .ForMember(dst => dst.PaymentDate,
+                opt => opt.MapFrom(src => DateTime.Now.ToString(CultureInfo.InvariantCulture)))
+            .ForMember(dst => dst.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber));
+    }
+}
